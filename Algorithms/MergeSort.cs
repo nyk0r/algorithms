07c9e4eda582d092db0c6129643b007fb26328f9
Tsalik Mozgunov <tmozgunov@accesssoftek.com>
2015-07-03 18:18:23 +0500

    Add merge sort

--- Algorithms/MergeSort.cs
+++ Algorithms/MergeSort.cs
@@ -1,35 +1,95 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Algorithms {
     public static class MergeSort {
-        internal static IEnumerable<T> Merge<T>(IEnumerable<T> seq1, IEnumerable<T> seq2, Sorting dir = Sorting.Asc) where T : IComparable<T> {
-            throw new NotImplementedException();
+        class SourceWrapper<T> {
+            private IList<T> _list;
+            private int _current, _end;
 
-            var comparer = SequenceUtils.GetComparer<T>(dir);
-            var result = new List<T>();
-            IEnumerator<T> enum1 = seq1.GetEnumerator(), enum2 = seq2.GetEnumerator();
-            try {
-                while (true) {
-                    bool moreSeq1 = enum1.MoveNext(), moreSeq2 = enum2.MoveNext();
-                    if (moreSeq1 && !moreSeq2) {
-                        
-                    } else if (!moreSeq1 && moreSeq2) {
+            public SourceWrapper(IList<T> list, int begin, int end) {
+                _list = list;
+                _current = begin;
+                _end = end;
+            }
 
-                    } else if (moreSeq1 && moreSeq2) {
-                        
-                    } else {
-                        break;
-                    }
+            public bool HasMore {
+                get {
+                    return _current < _end;
                 }
-            } finally {
-                enum1.Dispose();
-                enum2.Dispose();
+            }
+
+            public T Peak {
+                get { return _list[_current]; }
+            }
+
+            public T GetPeak() {
+                return _list[_current++];
             }
         }
 
-        public static void Sort<T>(IList<T> seq) where T : IComparable<T> {
-            throw new NotImplementedException();
+        private static Func<T, T, bool> GetSwapPrdicate<T>(Sorting dir) where T : IComparable<T> {
+            return
+                dir == Sorting.Asc
+                    ? (Func<T, T, bool>) ((a, b) => a.CompareTo(b) > 0)
+                    : (a, b) => a.CompareTo(b) < 0;
+        }
+
+        internal static void Merge<T>(IList<T> seq, int begin, int end, IList<T> buffer, Sorting dir) where T : IComparable<T> {
+            int middle = (begin + end)/2;
+            SourceWrapper<T> left = new SourceWrapper<T>(seq, begin, middle), right = new SourceWrapper<T>(seq, middle, end);
+            var swapPredicate = GetSwapPrdicate<T>(dir);
+            buffer.Clear();
+            while (left.HasMore || right.HasMore) {
+                if (!left.HasMore || right.HasMore && swapPredicate(left.Peak, right.Peak)) {
+                    buffer.Add(right.GetPeak());
+                } else {
+                    buffer.Add(left.GetPeak());
+                }
+            }
+
+            for (var idx = begin; idx < end; idx++) {
+                seq[idx] = buffer[idx - begin];
+            }
+        }
+
+        private static void SortTopDown<T>(IList<T> seq, int begin, int end, IList<T> buffer, Sorting dir) where T : IComparable<T> {
+            if (end - begin <= 1) {
+                return;
+            }
+
+            if (end - begin == 2) {
+                var swapPredicate = GetSwapPrdicate<T>(dir);
+                if (swapPredicate(seq[begin], seq[end - 1])) {
+                    var tmp = seq[begin];
+                    seq[begin] = seq[end - 1];
+                    seq[end - 1] = tmp;
+                }
+            }
+
+            var middle = (begin + end)/2;
+            SortTopDown(seq, begin, middle, buffer, dir);
+            SortTopDown(seq, middle, end, buffer, dir);
+            Merge(seq, begin, end, buffer, dir);
+        }
+
+        public static void SortTopDown<T>(IList<T> seq, Sorting dir = Sorting.Asc) where T : IComparable<T> {
+            SortTopDown(seq, 0, seq.Count, new List<T>(seq.Count), dir);
+        }
+
+        public static void SortBottomUp<T>(IList<T> seq, Sorting dir = Sorting.Asc) where T : IComparable<T> {
+            /*if (seq.Count <= 1) {
+                return;
+            }
+
+            var buffer = new List<T>(seq.Count);
+            for (var width = 1; width <= seq.Count; width *= 2) {
+                for (var part = 0; part < seq.Count/width; part++) {
+                    int begin = part*width, end = (part + 1)*width;
+                    Merge(seq, begin, end, buffer, dir);
+                }
+            }*/
         }
     }
 }
--- AlgorithmsTests/MergeSortTest.cs
+++ AlgorithmsTests/MergeSortTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Data;
 using NUnit.Framework;
 using Algorithms;
@@ -7,9 +8,123 @@ using Algorithms;
 namespace AlgorithmsTests {
     [TestFixture]
     internal class MergeSortTest {
-        /*[Test]
-        public void TestMergeSort() {
-            
-        }*/
+        [Test]
+        public void TestMergeAsc() {
+            var sequences = new[] {
+                // edge cases
+                new double[] { },
+                new double[] { 1 },
+
+                // warming up
+                new double[] { 1, 2 },
+                new double[] { 1, 2, 3},
+                new double[] { 1, 2, 3, 4},
+
+                // even
+                new double[] { 1, 3, 5, 2, 4, 6 },
+                new double[] { 2, 4, 6, 1, 3, 5 },
+                new double[] { 1, 1, 3, 5, 5, 2, 4, 4, 6, 6},
+                new double[] { 2, 2, 4, 4, 6, 1, 1, 3, 5, 5 },
+
+                // odd
+                new double[] { 1, 3, 5, 2, 4, 6, 7 },
+                new double[] { 2, 4, 6, 1, 3, 5, 7 },
+                new double[] { 1, 1, 3, 5, 5, 2, 4, 4, 6, 6, 6},
+                new double[] { 2, 2, 4, 4, 6, 1, 1, 3, 3, 5, 5 }
+            };
+
+            foreach (var seq in sequences) {
+                var buffer = new double[seq.Length];
+                MergeSort.Merge(seq, 0, seq.Length, buffer, Sorting.Asc);
+                Assert.True(SequenceUtils.IsOrdered(seq, Ordering.LtOrEq));
+            }
+        }
+
+        [Test]
+        public void TestMergeDesc() {
+            var sequences = new[] {
+                // edge cases
+                new double[] { },
+                new double[] { 1 },
+
+                // warming up
+                new double[] { 1, 2 },
+                new double[] { 3, 2, 1 },
+                new double[] { 4, 3, 2, 1 },
+
+                // even
+                new double[] { 5, 3, 1, 6, 4, 2 },
+                new double[] { 6, 4, 2, 5, 3, 1 },
+                new double[] { 5, 5, 3, 1, 1, 6, 4, 4, 2, 2 },
+                new double[] { 6, 6, 4, 4, 2, 5, 5, 3, 1, 1 },
+
+                // odd
+                new double[] { 5, 3, 1, 7, 6, 4, 2 },
+                new double[] { 6, 4, 2, 7, 5, 3, 1},
+                new double[] { 5, 5, 3, 1, 1, 6, 4, 4, 2, 2, 2 },
+                new double[] { 6, 4, 4, 2, 2, 5, 5, 3, 3, 1, 1 }
+            };
+
+            foreach (var seq in sequences) {
+                var buffer = new double[seq.Length];
+                MergeSort.Merge(seq, 0, seq.Length, buffer, Sorting.Desc);
+                Assert.True(SequenceUtils.IsOrdered(seq, Ordering.GtOrEq));
+            }
+        }
+
+        private static double[][] GetFixtures() {
+            var rnd = new Random();
+            return new [] {
+                // edge cases
+                new double[] { },
+                new double[] { 1 },
+
+                // warming up
+                new double[] { 1, 2 },
+                new double[] { 3, 1, 2 },
+                new double[] { 4, 2, 3, 5 },
+
+                // even
+                Enumerable.Range(0, 100).Select(_ => Math.Round(rnd.NextDouble() * 100)).ToArray(),
+                
+                // odd
+                Enumerable.Range(0, 99).Select(_ => Math.Round(rnd.NextDouble() * 100)).ToArray()
+            };
+        }
+
+        [Test]
+        public void TestTopDownSortAsc() {
+            foreach (var seq in GetFixtures()) {
+                MergeSort.SortTopDown(seq, Sorting.Asc);
+                Assert.True(SequenceUtils.IsOrdered(seq, Ordering.LtOrEq));
+            }
+        }
+
+        [Test]
+        public void TestTopDownSortDsc() {
+            foreach (var seq in GetFixtures()) {
+                MergeSort.SortTopDown(seq, Sorting.Desc);
+                Assert.True(SequenceUtils.IsOrdered(seq, Ordering.GtOrEq));
+            }
+        }
+
+        [Test]
+        [Ignore]
+        public void TestBottomUpSortAsc() {
+            foreach (var seq in GetFixtures()) {
+                MergeSort.SortBottomUp(seq, Sorting.Asc);
+                Assert.True(SequenceUtils.IsOrdered(seq, Ordering.LtOrEq));
+            }
+        }
+
+        [Test]
+        [Ignore]
+        public void TestBottomUpSortDsc()
+        {
+            foreach (var seq in GetFixtures()) {
+                MergeSort.SortBottomUp(seq, Sorting.Desc);
+                Assert.True(SequenceUtils.IsOrdered(seq, Ordering.GtOrEq));
+            }
+        }
     }
 }
