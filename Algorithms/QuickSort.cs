1f414274b8a14efad365f81acf283f135b420658
Tsalik Mozgunov <tmozgunov@accesssoftek.com>
2015-07-21 20:26:05 +0500

    Add 3-way quick sort

--- Algorithms/QuickSort.cs
+++ Algorithms/QuickSort.cs
@@ -41,5 +41,47 @@ namespace Algorithms {
         public static void Sort2<T>(IList<T> seq, Sorting dir = Sorting.Asc) where T:IComparable<T> {
             Sort2(seq, dir, Comparer<T>.Default);
         }
+
+        internal static Tuple<int, int> Partition3<T>(IList<T> seq, int begin, int end, Func<T, T, bool> areOutOfOrder) {
+            if (begin >= end) {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int lo = begin, idx = begin, hi = end;
+            T partitioner = seq[begin];
+            while (idx <= hi) {
+                if (areOutOfOrder(partitioner, seq[idx])) {
+                    SequenceUtils.Swap(seq, lo, idx);
+                    ++lo;
+                    ++idx;
+                } else if (areOutOfOrder(seq[idx], partitioner)) {
+                    SequenceUtils.Swap(seq, hi, idx);
+                    --hi;
+                } else {
+                    ++idx;
+                }
+            }
+
+            return Tuple.Create(lo, hi);
+        }
+
+        private static void Sort3<T>(IList<T> seq, int begin, int end, Sorting dir, IComparer<T> comparer, Func<T, T, bool> areOutOfOrder) {
+            if (end - begin <= SIMPLE_SORT_THRESHOLD) {
+                ElementarySorts.Insertion(seq, dir, comparer);
+                return;
+            }
+            var pivots = Partition3(seq, begin, end, areOutOfOrder);
+            Sort3(seq, begin, pivots.Item1 - 1, dir, comparer, areOutOfOrder);
+            Sort3(seq, pivots.Item2 + 1, end, dir, comparer, areOutOfOrder);
+        }
+
+        public static void Sort3<T>(IList<T> seq, Sorting dir, IComparer<T> comparer) {
+            SequenceUtils.Shuffle(seq);
+            Sort3(seq, 0, seq.Count - 1, dir, comparer, SequenceUtils.GetOutOfOrderPrdicate(dir, comparer));
+        }
+
+        public static void Sort3<T>(IList<T> seq, Sorting dir = Sorting.Asc) where T : IComparable<T> {
+            Sort3(seq, dir, Comparer<T>.Default);
+        }
     }
 }
--- AlgorithmsTests/QuickSortTest.cs
+++ AlgorithmsTests/QuickSortTest.cs
@@ -5,132 +5,141 @@ using Algorithms;
 
 namespace AlgorithmsTests {
     internal class QuickSortTest : SortTest {
-        private sealed class PartitioningResult {
-            public int Point { get; private set; }
+        private class PartitioningResult {
             public double[] Sequnece { get; private set; }
             public Sorting Sorting { get; private set; }
 
-            public PartitioningResult(int point, double[] sequnece, Sorting sorting) {
-                Point = point;
+            public PartitioningResult(double[] sequnece, Sorting sorting) {
                 Sequnece = sequnece;
                 Sorting = sorting;
             }
         }
 
+        private sealed class Partitioning2Result : PartitioningResult {
+            public int Point { get; private set; }
+
+            public Partitioning2Result(int point, double[] sequnece, Sorting sorting)
+                : base(sequnece, sorting) {
+                Point = point;
+            }
+        }
+
+        private sealed class Partitioning3Result : PartitioningResult {
+            public Tuple<int, int> Points { get; private set; }
+
+            public Partitioning3Result(Tuple<int, int> point, double[] sequnece, Sorting sorting)
+                : base(sequnece, sorting) {
+                Points = point;
+            }
+        }
+
         [Test]
         public void TestPartition2() {
-            var fixtures = new Dictionary<double[], PartitioningResult[]> {
+            var fixtures = new Dictionary<double[], Partitioning2Result[]> {
                 {
                     new [] { 1.0, 1.0 }, 
                     new [] 
                     { 
-                        new PartitioningResult(0, new [] { 1.0, 1.0 }, Sorting.Asc),
-                        new PartitioningResult(0, new [] { 1.0, 1.0 }, Sorting.Desc)
+                        new Partitioning2Result(0, new [] { 1.0, 1.0 }, Sorting.Asc),
+                        new Partitioning2Result(0, new [] { 1.0, 1.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 1.0, 2.0 }, 
                     new [] {
-                        new PartitioningResult(0, new [] { 1.0, 2.0 }, Sorting.Asc),
-                        new PartitioningResult(1, new [] { 2.0, 1.0 }, Sorting.Desc)
+                        new Partitioning2Result(0, new [] { 1.0, 2.0 }, Sorting.Asc),
+                        new Partitioning2Result(1, new [] { 2.0, 1.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 2.0, 1.0 }, 
                     new [] {
-                        new PartitioningResult(1, new [] { 1.0, 2.0 }, Sorting.Asc),
-                        new PartitioningResult(0, new [] { 2.0, 1.0 }, Sorting.Desc)
+                        new Partitioning2Result(1, new [] { 1.0, 2.0 }, Sorting.Asc),
+                        new Partitioning2Result(0, new [] { 2.0, 1.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 0.0, 0.0, 0.0 }, 
                     new [] {
-                        new PartitioningResult(0, new [] { 0.0, 0.0, 0.0 }, Sorting.Asc),
-                        new PartitioningResult(0, new [] { 0.0, 0.0, 0.0 }, Sorting.Desc)
+                        new Partitioning2Result(0, new [] { 0.0, 0.0, 0.0 }, Sorting.Asc),
+                        new Partitioning2Result(0, new [] { 0.0, 0.0, 0.0 }, Sorting.Desc)
                     }
                 },
                 { 
                     new [] { 1.0, 2.0, 3.0 }, 
                     new [] {
-                        new PartitioningResult(0, new [] { 1.0, 2.0, 3.0 }, Sorting.Asc),
-                        new PartitioningResult(2, new [] { 3.0, 2.0, 1.0 }, Sorting.Desc)
+                        new Partitioning2Result(0, new [] { 1.0, 2.0, 3.0 }, Sorting.Asc),
+                        new Partitioning2Result(2, new [] { 3.0, 2.0, 1.0 }, Sorting.Desc)
                     } 
                 }, 
                 {
                     new [] { 1.0, 3.0, 2.0 }, 
                     new [] {
-                        new PartitioningResult(0, new [] { 1.0, 3.0, 2.0 }, Sorting.Asc),
-                        new PartitioningResult(2, new [] { 2.0, 3.0, 1.0 }, Sorting.Desc)
+                        new Partitioning2Result(0, new [] { 1.0, 3.0, 2.0 }, Sorting.Asc),
+                        new Partitioning2Result(2, new [] { 2.0, 3.0, 1.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 2.0, 1.0, 3.0 }, 
                     new [] {
-                        new PartitioningResult(1, new [] { 1.0, 2.0, 3.0 }, Sorting.Asc),
-                        new PartitioningResult(1, new [] { 3.0, 2.0, 1.0 }, Sorting.Desc)
+                        new Partitioning2Result(1, new [] { 1.0, 2.0, 3.0 }, Sorting.Asc),
+                        new Partitioning2Result(1, new [] { 3.0, 2.0, 1.0 }, Sorting.Desc)
                     }
-                }, 
-                {
-                    new [] { 2.0, 1.0, 3.0 }, 
-                    new [] {
-                        new PartitioningResult(1, new [] { 1.0, 2.0, 3.0 }, Sorting.Asc),
-                        new PartitioningResult(1, new [] { 3.0, 2.0, 1.0 }, Sorting.Desc)
-                    }
-                }, 
+                },  
                 {
                     new [] { 3.0, 2.0, 1.0 }, 
                     new [] {
-                        new PartitioningResult(2, new [] { 1.0, 2.0, 3.0 }, Sorting.Asc),
-                        new PartitioningResult(0, new [] { 3.0, 2.0, 1.0 }, Sorting.Desc)
+                        new Partitioning2Result(2, new [] { 1.0, 2.0, 3.0 }, Sorting.Asc),
+                        new Partitioning2Result(0, new [] { 3.0, 2.0, 1.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 3.0, 1.0, 2.0 }, 
                     new[] {
-                        new PartitioningResult(2, new [] { 2.0, 1.0, 3.0 }, Sorting.Asc),
-                        new PartitioningResult(0, new [] { 3.0, 1.0, 2.0 }, Sorting.Desc)
+                        new Partitioning2Result(2, new [] { 2.0, 1.0, 3.0 }, Sorting.Asc),
+                        new Partitioning2Result(0, new [] { 3.0, 1.0, 2.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 7.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0 }, 
                     new [] {
-                        new PartitioningResult(6, new [] { 0.0, 3.0, 2.0, 1.0, 5.0, 1.0, 7.0, 9.0, 8.0 }, Sorting.Asc),
-                        new PartitioningResult(2, new [] { 8.0, 9.0, 7.0, 1.0, 2.0, 3.0, 0.0, 1.0, 5.0 }, Sorting.Desc)
+                        new Partitioning2Result(6, new [] { 0.0, 3.0, 2.0, 1.0, 5.0, 1.0, 7.0, 9.0, 8.0 }, Sorting.Asc),
+                        new Partitioning2Result(2, new [] { 8.0, 9.0, 7.0, 1.0, 2.0, 3.0, 0.0, 1.0, 5.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 0.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0 }, 
                     new [] {
-                        new PartitioningResult(0, new [] { 0.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0 }, Sorting.Asc),
-                        new PartitioningResult(8, new [] { 5.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 0.0 }, Sorting.Desc)
+                        new Partitioning2Result(0, new [] { 0.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0 }, Sorting.Asc),
+                        new Partitioning2Result(8, new [] { 5.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 0.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 9.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0 }, 
                     new [] {
-                        new PartitioningResult(8, new [] { 5.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 9.0 }, Sorting.Asc),
-                        new PartitioningResult(0, new [] { 9.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0 }, Sorting.Desc)
+                        new Partitioning2Result(8, new [] { 5.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 9.0 }, Sorting.Asc),
+                        new Partitioning2Result(0, new [] { 9.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 7.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 4.0 }, 
                     new [] {
-                        new PartitioningResult(7, new [] { 1.0, 3.0, 2.0, 1.0, 4.0, 5.0, 0.0, 7.0, 9.0, 8.0 }, Sorting.Asc),
-                        new PartitioningResult(2, new [] { 8.0, 9.0, 7.0, 1.0, 2.0, 3.0, 0.0, 1.0, 5.0, 4.0 }, Sorting.Desc)
+                        new Partitioning2Result(7, new [] { 1.0, 3.0, 2.0, 1.0, 4.0, 5.0, 0.0, 7.0, 9.0, 8.0 }, Sorting.Asc),
+                        new Partitioning2Result(2, new [] { 8.0, 9.0, 7.0, 1.0, 2.0, 3.0, 0.0, 1.0, 5.0, 4.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 0.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 4.0 }, 
                     new [] {
-                        new PartitioningResult(0, new [] { 0.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 4.0 }, Sorting.Asc),
-                        new PartitioningResult(9, new [] { 4.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 0.0 }, Sorting.Desc)
+                        new Partitioning2Result(0, new [] { 0.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 4.0 }, Sorting.Asc),
+                        new Partitioning2Result(9, new [] { 4.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 0.0 }, Sorting.Desc)
                     }
                 }, 
                 {
                     new [] { 9.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 4.0 }, 
                     new [] {
-                        new PartitioningResult(9, new [] { 4.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 9.0 }, Sorting.Asc),
-                        new PartitioningResult(0, new [] { 9.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 4.0 }, Sorting.Desc)
+                        new Partitioning2Result(9, new [] { 4.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 9.0 }, Sorting.Asc),
+                        new Partitioning2Result(0, new [] { 9.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 4.0 }, Sorting.Desc)
                     }
                 },
             };
@@ -158,6 +167,143 @@ namespace AlgorithmsTests {
         [Test]
         public void TestSort2Desc() {
             TestSortDesc(QuickSort.Sort2);
+        }
+
+        [Test]
+        public void TestPartition3() {
+            var fixtures = new Dictionary<double[], Partitioning3Result[]> {
+                {
+                    new [] { 1.0, 1.0 }, 
+                    new [] 
+                    { 
+                        new Partitioning3Result(Tuple.Create(0, 1), new [] { 1.0, 1.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(0, 1), new [] { 1.0, 1.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 1.0, 2.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(0, 0), new [] { 1.0, 2.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(1, 1), new [] { 2.0, 1.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 2.0, 1.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(1, 1), new [] { 1.0, 2.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(0, 0), new [] { 2.0, 1.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 0.0, 0.0, 0.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(0, 2), new [] { 0.0, 0.0, 0.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(0, 2), new [] { 0.0, 0.0, 0.0 }, Sorting.Desc)
+                    }
+                },
+                { 
+                    new [] { 1.0, 2.0, 3.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(0, 0), new [] { 1.0, 3.0, 2.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(2, 2), new [] { 2.0, 3.0, 1.0 }, Sorting.Desc)
+                    } 
+                }, 
+                {
+                    new [] { 1.0, 3.0, 2.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(0, 0), new [] { 1.0, 2.0, 3.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(2, 2), new [] { 3.0, 2.0, 1.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 2.0, 1.0, 3.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(1, 1), new [] { 1.0, 2.0, 3.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(1, 1), new [] { 3.0, 2.0, 1.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 3.0, 2.0, 1.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(2, 2), new [] { 2.0, 1.0, 3.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(0, 0), new [] { 3.0, 1.0, 2.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 3.0, 1.0, 2.0 }, 
+                    new[] {
+                        new Partitioning3Result(Tuple.Create(2, 2), new [] { 1.0, 2.0, 3.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(0, 0), new [] { 3.0, 2.0, 1.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 7.0, 3.0, 2.0, 7.0, 8.0, 9.0, 0.0, 9.0, 5.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(4, 5), new [] { 3.0, 2.0, 5.0, 0.0, 7.0, 7.0, 9.0, 9.0, 8.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(3, 4), new [] { 9.0, 9.0, 8.0, 7.0, 7.0, 0.0, 2.0, 5.0, 3.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 0.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(0, 1), new [] { 0.0, 0.0, 1.0, 8.0, 9.0, 2.0, 1.0, 5.0, 3.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(7, 8), new [] { 3.0, 2.0, 1.0, 8.0, 9.0, 1.0, 5.0, 0.0, 0.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 9.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(7, 8), new [] { 3.0, 2.0, 1.0, 8.0, 0.0, 1.0, 5.0, 9.0, 9.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(0, 1), new [] { 9.0, 9.0, 1.0, 8.0, 2.0, 0.0, 1.0, 5.0, 3.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 7.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 7.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(6, 7), new [] { 3.0, 2.0, 1.0, 5.0, 0.0, 1.0, 7.0, 7.0, 9.0, 8.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(2, 3), new [] { 9.0, 8.0, 7.0, 7.0, 1.0, 0.0, 1.0, 5.0, 2.0, 3.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 0.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 0.0, 9.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(0, 2), new [] {  0.0, 0.0, 0.0, 8.0, 9.0, 1.0, 1.0, 2.0, 9.0, 3.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(7, 9), new [] { 3.0, 2.0, 1.0, 8.0, 9.0, 1.0, 9.0, 0.0, 0.0, 0.0 }, Sorting.Desc)
+                    }
+                }, 
+                {
+                    new [] { 9.0, 3.0, 2.0, 1.0, 8.0, 9.0, 0.0, 1.0, 5.0, 4.0 }, 
+                    new [] {
+                        new Partitioning3Result(Tuple.Create(8, 9), new [] { 3.0, 2.0, 1.0, 8.0, 0.0, 1.0, 5.0, 4.0, 9.0, 9.0 }, Sorting.Asc),
+                        new Partitioning3Result(Tuple.Create(0, 1), new [] { 9.0, 9.0, 1.0, 8.0, 2.0, 0.0, 1.0, 5.0, 4.0, 3.0 }, Sorting.Desc)
+                    }
+                },
+            };
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => QuickSort.Partition3(new double[] { }, 0, -1, SequenceUtils.GetOutOfOrderPrdicate<double>(Sorting.Asc)));
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => QuickSort.Partition3(new double[] { 0 }, 0, 0, SequenceUtils.GetOutOfOrderPrdicate<double>(Sorting.Asc)));
+
+            foreach (var fixture in fixtures) {
+                foreach (var result in fixture.Value) {
+                    var arr = (double[]) fixture.Key.Clone();
+                    var points = QuickSort.Partition3(arr, 0, arr.Length - 1,
+                        SequenceUtils.GetOutOfOrderPrdicate<double>(result.Sorting));
+                    Assert.AreEqual(points, result.Points);
+                    CollectionAssert.AreEqual(arr, result.Sequnece);
+                }
+            }
+        }
+
+        [Test]
+        public void TestSort3Asc() {
+            TestSortAsc(QuickSort.Sort3);
+        }
+
+        [Test]
+        public void TestSort3Desc() {
+            TestSortDesc(QuickSort.Sort3);
         }
     }
 }
